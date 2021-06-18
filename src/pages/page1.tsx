8d3958c6e6286c919a091dc8617f475c6f96ef67
mildtotoro <pimrampha.m@gmail.com>
2021-06-18 18:40:23 +0700

    update file name css

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/page.module.css";
 import { getValue } from "../api/service";
 import { useState } from "react";
 import { useRouter } from "next/router";
--- src/pages/page2.tsx
+++ src/pages/page2.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/page.module.css";
 import { useRouter } from "next/router";
 
 export default function PageTwo({ state }) {
